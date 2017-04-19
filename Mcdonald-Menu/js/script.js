22ac7fa48d35e894904762d6a7687862900bedcd
sc2778 <sc2778@cornell.edu>
2017-04-19 13:23:07 -0400

    inital commit

---
+++ Mcdonald-Menu/js/script.js
@@ -0,0 +1,79 @@
+(function($) {
+  "use strict";
+
+//------------------------------------- Waiting for the entire site to load ------------------------------------------------//
+
+
+$(document).ready(function(){
+	
+	
+if (!(/android|blackberry|windows phone|iphone|ipod/i).test(navigator.userAgent.toLowerCase())) {
+
+	var header = $('.mainHeader'),
+		pos = header.offset();
+
+		$(window).scroll(function(){
+			if($(this).scrollTop() > pos.top+20 && header.hasClass('default')){
+				header.fadeOut('fast', function(){
+					$(this).removeClass('default').addClass('switchedHeader').slideDown(200);
+				});
+			} else if($(this).scrollTop() <= pos.top+20 && header.hasClass('switchedHeader')){
+				header.slideUp(200, function(){
+					$(this).removeClass('switchedHeader').addClass('default').fadeIn(200);
+				});
+			}
+	});
+	
+}
+	
+	
+//------------------------------------- Navigation setup ------------------------------------------------//
+
+$('a.scroll').smoothScroll({
+        speed: 800
+        
+});
+
+$('button.scroll').smoothScroll({
+	top:800,
+	left:0
+});
+
+//------------------------------------- End navigation setup ------------------------------------------------//
+
+
+
+
+
+//---------------------------------- Portfolio -----------------------------------------//
+
+$(".itemDesc, .prjLink").css({ opacity: 0 });
+
+//--------------------------------- Hover animation for the elements of the portfolio --------------------------------//
+				
+	
+$('.itemDesc, .prjLink').hover( function(){ 
+	$(this).stop().animate({ opacity: 1 }, 'slow');
+}, function(){ 
+	$(this).stop().animate({ opacity: 0 }, 'slow'); 
+});
+
+	$('.itemDesc, .prjLink').hover(function () {
+    var projDesc = $(this).find('.itemDesc, .latestDesc, .prjLink');
+    var offset = ($(this).height() / 2) - (projDesc.height() / 2);
+    $(this).find('.itemDescInner, .prjLinkInner').css('padding-top', offset -30);
+});
+			
+
+//--------------------------------- End hover animation for the elements of the portfolio --------------------------------//
+
+				
+//-----------------------------------End initilaizing fancybox for the portfolio-------------------------------------------------//
+
+
+
+
+
+});
+
+})(jQuery);
